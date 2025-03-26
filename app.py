3e9c65b1213889dae54dda2aa38e0ef2d5bffe48
Meet1832 <93570110+Meet1832@users.noreply.github.com>
2025-03-26 14:45:17 +0530

    app.py

--- app.py
+++ app.py
@@ -560,6 +560,11 @@ def download_report():
     )
 
 if __name__ == '__main__':
+    @app.route("/")
+    def home():
+        return "Hello from Flask on Vercel!"
+
+    app.run(host="0.0.0.0", port=8080)
     # Get port from environment variable or use default
     port = int(os.environ.get('PORT', 5000))
     
